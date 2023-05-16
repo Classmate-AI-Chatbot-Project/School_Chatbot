457bf1423bea7acd21dd96f70c552445601967e2
thisisdana49 <thisisdana49@gmail.com>
2023-05-17 01:59:19 +0900

    📰 Feat: SideBar 추가
    - SideBar component 추가
    - TopBar와 연결 완료

--- frontend/schoollog-react-app/src/component/topbar/TopBar.tsx
+++ frontend/schoollog-react-app/src/component/topbar/TopBar.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './TopBar.css'
+import SideBar from '../SideBar.tsx/SideBar';
 import { ReactComponent as HamburgerIcon } from '../../assets/top-bar-hamburger.svg'
 import { ReactComponent as PersonIcon } from '../../assets/top-bar-person.svg'
 
 
-function TopBar() {
+function TopBar({ isOpen, setIsOpen }: { isOpen: boolean; setIsOpen: any }) {
+  const toggleSide = () => {
+    setIsOpen(true);
+  };
+
   return (
     <div className="topnav">
-      <HamburgerIcon/>
+      <HamburgerIcon role="button" onClick={toggleSide}/>
       <a style={{color: 'black'}}>Logo</a>
       <PersonIcon/>
+
     </div>
   );
 }
