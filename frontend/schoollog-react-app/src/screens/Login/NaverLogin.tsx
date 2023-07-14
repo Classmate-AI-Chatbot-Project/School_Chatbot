15f25ba01bd56c232e787240e2650046553eb8a4
thisisdana49 <thisisdana49@gmail.com>
2023-07-14 16:55:57 +0900

    📰 [Refactor] 클라이언트 ID 및 API 환경변수 설정
    - 프로젝트 외부에 노출하지 말아야 할 정보(Api key, Password 등)를 보호하기 위한 환경변수 파일 추가 및 설정

--- frontend/schoollog-react-app/src/screens/Login/NaverLogin.tsx
+++ frontend/schoollog-react-app/src/screens/Login/NaverLogin.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const NaverLogin= () => {
-  const NAVER_CLIENT_ID = 'o20GjxDlH8cHP6nVVDTM';
+  const CLIENT_ID = 'o20GjxDlH8cHP6nVVDTM';
   const REDIRECT_URI = 'http://localhost:3000/naverLogin';
   const NAVER_STATE = 'false';
 
-  const NAVER_URI = `https://nid.naver.com/oauth2.0/authorize?client_id=${NAVER_CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&state=${NAVER_STATE}`;
+  const NAVER_URI = `https://nid.naver.com/oauth2.0/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&state=${NAVER_STATE}`;
 
 
   return (
