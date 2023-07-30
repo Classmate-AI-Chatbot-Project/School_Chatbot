3bd0c95c037e2d4f2cfc7d1a1f04a8c327cf5ad5
김수현 <gamjatan9@gmail.com>
2023-07-31 01:20:33 +0900

    [Feat] 수정

--- frontend/schoollog-react-app/src/screens/Message/Message.tsx
+++ frontend/schoollog-react-app/src/screens/Message/Message.tsx
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 import { Link } from 'react-router-dom';
 import "../Chat/Chat.css"
 import "./Message.css"
+import "../Chat/Modal.css";
 import dummyProfile from '../../assets/dummy-profile-img.png'
 import { ReactComponent as MesBegin } from '../../assets/mes-begin.svg'
 import { formatDate, currentDate } from "../Chat/Chat";
@@ -13,10 +14,35 @@ interface MessageInput {
   answer?: string;
 }
 
+interface ModalProps {
+  open: boolean;
+  close: () => void;
+}
+
+const ProfileModal: React.FC<ModalProps> = (props) => {
+  const { open, close } = props;
+  const handleCloseButtonClick = () => {
+    close();
+  };
+  return (
+    <div className={open ? 'openModal modal' : 'modal'}>
+      <section className="MessageModal-contentBox">  
+        <div className="Modal-main">
+          <img className="Mes-modalProfile" src={dummyProfile} />
+          <div className="Mes-modalName">조다은 선생님</div>
+          <div className="Mes-modalSchool">덕성여자고등학교</div>
+          <button className="Modal-gotoResult"  onClick={handleCloseButtonClick}>확인</button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
 function Message() {
   const [message, setMessage] = useState("");
   const [chatLog, setChatLog] = useState<MessageInput[]>([]);
   const [showDate, setShowDate] = useState(false);
+  const [profileModalOpen, setProfileModalOpen] = useState(false);
 
   const generateId = () => {
     return Date.now(); // ChatAnswer에 부여되는 id. 로딩 표현하기 위해 사용
@@ -54,6 +80,13 @@ function Message() {
     }
   };
 
+  const openProfileModal = () => {
+    setProfileModalOpen(true);
+  };
+  const closeProfileModal = () => {
+    setProfileModalOpen(false);
+  };
+
   return (
     <div className="Chat-Fullbox">
       <header className="Chat-Contentbox">
@@ -76,7 +109,9 @@ function Message() {
                 <span className="Chat-message">{chat.message}</span>
               </div>
               <div className="Mes-A1">
-                  <span className="Mes-profile"><img src={dummyProfile}/></span>
+                  <span className="Mes-profile">
+                      <img onClick={openProfileModal} src={dummyProfile} />
+                  </span>
                   <span className="Mes-A2">
                     <div className="Mes-name">조다은 선생님 </div>
                     <div className="Mes-answer">답변예시 답변예시 답변예시 답변예시 답변예시 답변예시 답변예시 </div>
@@ -100,6 +135,7 @@ function Message() {
           <button className="Chat-Send-btn" onClick={handleSubmit}></button>
         </div> 
       </header>
+      <ProfileModal open={profileModalOpen} close={() => setProfileModalOpen(false)} />
     </div>
   );
 }
